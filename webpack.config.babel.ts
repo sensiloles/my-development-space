5f2827d5b699f1abed322097ff7cd10ffc0048cb
Alexey Filippov <sensiloles@gmail.com>
2021-08-19 00:41:25 +0700

    Initialize project setup

---
+++ webpack.config.babel.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: {
+      app: path.join(__dirname, 'src', 'index.tsx')
+  },
+  target: 'web',
+  resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+      rules: [
+          {
+              test: /\.(js|jsx)$/,
+              use: "babel-loader",
+              exclude: /node_modules/,
+          },
+          {
+              test: /\.(ts|tsx)$/,
+              use: 'ts-loader',
+              exclude: /node_modules/
+          }
+      ],
+  },
+  output: {
+      filename: 'main.bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: path.join(__dirname, 'src', 'index.html')
+      })
+  ]
+}
+
+export default config;
